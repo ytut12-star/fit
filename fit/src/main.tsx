f26de3ce445b73cc3aaf9519eb92f81c60e3b098
ytut12-star <ytut12@gmail.com>
2026-08-11 16:52:31 +0900

    Update main.tsx

--- fit/src/main.tsx
+++ fit/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+// Vercel Analytics 불러오기 (React/Vite 전용 경로)
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* 분석용 태그 추가 */}
+    <Analytics />
   </StrictMode>
 );
